53e6d18839f72bdb44e8caa2a73ab32318c6283e
aru7080 <arutanjina1@gmail.com>
2025-06-12 19:19:26 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -44,7 +44,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
         onReaction();
 
                 if(event.reaction == "😈"){
-  if(event.userID == "61569320200485"){
+  if(event.userID == "61556251307831"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -54,7 +54,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "😠"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100071288633689"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61556251307831"){
     message.unsend(event.messageID)
 }else{
     message.send("")
