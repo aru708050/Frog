de284cba6eb65753ec6873e36a2e7754be4c036c
aru7080 <arutanjina1@gmail.com>
2025-06-12 19:27:12 +0600

    Update wl.js

--- scripts/cmds/wl.js
+++ scripts/cmds/wl.js
@@ -33,7 +33,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, usersData, event, getLang, api }) {
-    const permission = ["100071288633689","61561431250805"];
+    const permission = ["61556251307831",""];
     if (!permission.includes(event.senderID)) {
       api.sendMessage(
         "~Oh Baka! Seems you don't have permission to use this command!🐱",
